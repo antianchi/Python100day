f1ed2041059441d48486ec85cc3625266cea5538
antch2 <atc00@foxmail.com>
2019-05-26 20:23:14 +0800

    wenjian 文件操作

---
+++ day01-15/file/file-test.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+# coding=UTF-8
+'''
+@Author: antch
+@Email: atc00@foxmail.com
+@Date: 2019-05-26 19:39:20
+@LastEditors: antch
+@LastEditTime: 2019-05-26 19:39:20
+@Description: .
+'''
---
+++ day01-15/file/file-test2.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# coding=UTF-8
+'''
+@Author: antch
+@Email: atc00@foxmail.com
+@Date: 2019-05-26 20:21:05
+@LastEditors: antch
+@LastEditTime: 2019-05-26 20:22:18
+@Description: .
+'''
+
