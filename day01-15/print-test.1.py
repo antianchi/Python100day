0fb26bf7400909a4f9d719c4af9f364725de47ea
antch2 <atc00@foxmail.com>
2019-05-26 18:53:39 +0800

    print 打印中文测试，python2会出错，python3不会

---
+++ day01-15/print-test.1.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+#! python3
+"""
+Author: antch
+Email: atc00@foxmail.com
+
+date: 2019/5/26 18.49
+desc: print打印中文测试.
+
+"""
+# import sys
+
+# reload(sys)
+# sys.setdefaultencoding('utf8')
+
+print("你好")
