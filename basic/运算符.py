0e4ca12b46926244e3452fe0a592a79f43e00e1c
antch2 <atc00@foxmail.com>
2019-06-03 22:33:13 +0800

    对象，运算符

---
+++ basic/运算符.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+# coding=UTF-8
+#! python3
+'''
+
+Author: antch
+Email: atc00@foxmail.com
+Date: 2019-06-03 20:55:12
+LastEditors: antch
+LastEditTime: 2019-06-03 20:55:12
+Description: 运算符.
+
+
+/ 除法运算
+运算时返回浮点数
+// 整除
+只会保留计算后的整数部分
+**幂运算
+% 取模，求两个数相除的余数。
+
+==  != 比较的是值是否相等，而不是ID
+is 比较两个对象是否是同一个对象，比较的是ID
+is not
+
+
+
+
+'''
